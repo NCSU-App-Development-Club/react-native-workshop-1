6c11b1dbfaec7ef22bc7dafdab47535b7f4d08f8
Abel Lu <alu4@ncsu.edu>
2024-09-20 22:45:21 -0400

    completed checkpoint 1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Platform } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [count, setCount] = useState<number>(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Count: {count}{Platform.OS == "android" && ' '}</Text>
+      <Button title="Increase Count" onPress={() => setCount(count + 1)}/>
+      <Button title="Decrease Count" onPress={() => setCount(count - 1)}/>
       <StatusBar style="auto" />
     </View>
   );
